83fed67bac18c153d700be13099a700c013799f3
JianqiaoAirport <rommeldhy@163.com>
2018-09-05 14:41:44 +0900

    add global_variables and update config

--- random_agent.py
+++ random_agent.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 
 import config
+import global_variables
 
 
 class RandomAgent:
@@ -15,7 +16,7 @@ class RandomAgent:
     def get_action(self, state, temp=1e-3, return_prob=False):
         sensible_moves = state.legal_action_list
         # the pi vector returned by MCTS as in the alphaGo Zero paper
-        move_probs = np.zeros(len(config.ACTION_VECTOR_DICT.keys())-1)
+        move_probs = np.zeros(len(global_variables.ACTION_VECTOR_DICT.keys()) - 1)
         if len(sensible_moves) > 0:
             probs = np.random.dirichlet(np.ones(2*config.DIMENSIONALITY), size=1)
             acts = np.argmax(probs)
